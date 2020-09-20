1db36df28e7a4dabfa7d5414796d294455e94dee
n.rodriguezb <n.rodriguezb@alumnos.upm.es>
2020-09-20 23:14:13 +0200

    Last version

--- threadANALOG.cpp
+++ threadANALOG.cpp
@@ -3,7 +3,7 @@
 
 //SOIL MOISTURE
 AnalogIn soilmois(PA_0);
-float valueSM=0.0;
+float valueSM = 0.0;
 
 float max_soil = 0.0;
 float min_soil = 200.0;
@@ -15,7 +15,7 @@ float average_soil_hour = 0.0;
 
 //PHOTORESISTOR
 AnalogIn light(PA_4);
-float valueLight=0.0;
+float valueLight = 0.0;
 
 float max_light = 0.0;
 float min_light = 200.0;
@@ -27,84 +27,90 @@ float average_light_hour = 0.0;
 
 //COMMON VARIABLES
 Thread threadANALOG(osPriorityNormal, 512); // 1K stack size
-void ANALOG_thread(); 
+void ANALOG_thread();
 
 int contadorA = 1;
-int contador_maxA = 31; //Cada 30 seg el thread vuelve a ready, con lo cual 1h = 120 veces 30 segundos
+int contador_maxA = 31; //Every 30seg the thread returns to ready state, which means 1h = 120 times 30 seconds
 
 extern bool mode_change;
 extern int tiempo;
 
-void obtainMaxOrMinSoil(float value){
-	
-	if(value > max_soil){
+void obtainMaxOrMinSoil(float value)
+{
+
+	if (value > max_soil)
+	{
 		max_soil = value;
 	}
-	else if(value < min_soil){
+	else if (value < min_soil)
+	{
 		min_soil = value;
 	}
 }
 
-void obtainMaxOrMinLight(float value){
-	
-	if(value > max_light){
+void obtainMaxOrMinLight(float value)
+{
+
+	if (value > max_light)
+	{
 		max_light = value;
 	}
-	else if(value < min_light){
+	else if (value < min_light)
+	{
 		min_light = value;
 	}
 }
 
+void ANALOG_thread()
+{
 
-void ANALOG_thread() {
-		
-	while (true) {
-		Thread::wait(tiempo);  //ms
-		//Thread::wait(30000);  //ms		
-				
+	while (true)
+	{
+		Thread::wait(tiempo); //ms
+		//Thread::wait(30000);  //ms
+
 		//To obtain the percent is necessary to multiply by 100
 		//SOIL MOISTURE
-		valueSM=soilmois*100;
-			
+		valueSM = soilmois * 100;
+
 		//LIGHT SENSOR
-		valueLight = light*100;
-		
-		if(mode_change){
-			
+		valueLight = light * 100;
+
+		if (mode_change)
+		{
+
 			contadorA++;
 			//Reset contador when one hour passes
-			if(contadorA > contador_maxA){
+			if (contadorA > contador_maxA)
+			{
 				contadorA = 1;
-			
+
 				//Soil moisture
 				max_soil_hour = max_soil;
 				min_soil_hour = min_soil;
 				average_soil_hour = soil_average;
-				//Poner a cero max_soil, min_soil, soil_average
+				//put to zero max_soil, min_soil, soil_average
 				max_soil = 0.0;
 				min_soil = 0.0;
 				soil_average = 0.0;
-			
-			
+
 				//Light sensor
 				max_light_hour = max_light;
 				min_light_hour = min_light;
 				average_light_hour = light_average;
-				//Poner a cero max_light, min light, light average
+				//put to zero max_light, min light, light average
 				max_light = 0.0;
 				min_light = 0.0;
-				light_average = 0.0;	
+				light_average = 0.0;
 			}
-			
+
 			//SOIL MOISTURE
 			obtainMaxOrMinSoil(valueSM);
-			soil_average = (soil_average*(contadorA-1)+valueSM)/contadorA;
-			
+			soil_average = (soil_average * (contadorA - 1) + valueSM) / contadorA;
+
 			//LIGHT SENSOR
 			obtainMaxOrMinLight(valueLight);
-			light_average = (light_average*(contadorA-1)+valueLight)/contadorA;
-			
+			light_average = (light_average * (contadorA - 1) + valueLight) / contadorA;
 		}
-			
-  }
+	}
 }
--- threadI2C.cpp
+++ threadI2C.cpp
@@ -2,7 +2,7 @@
 #include "rtos.h"
 #include "MMA8451Q.h"
 
-#define MMA8451_I2C_ADDRESS (0x1C<<1)  //0x1d<<1  0x00<<1
+#define MMA8451_I2C_ADDRESS (0x1C << 1) //0x1d<<1  0x00<<1
 
 //INITIALIZE COMMON VARIABLES
 I2C i2c(I2C_SDA, I2C_SCL); //pins for I2C communication (SDA, SCL)
@@ -13,23 +13,23 @@ Thread threadI2C(osPriorityNormal, 512); // 1K stack size
 void I2C_thread();
 
 int contadorI2C = 1;
-int contador_maxI2C = 31;  //Cada 30 seg el thread vuelve a ready, con lo cual 1h = 120 veces 30 segundos
+int contador_maxI2C = 31; //Every 30seg the thread returns to ready state, which means 1h = 120 times 30 seconds
 
 extern int tiempo;
 
 //INITIALIZE COLOR SENSOR TCS34725
 DigitalOut ledColour(PH_1); // TCS34725 led. PA_10 is occuped
-DigitalOut ledR(PA_8); //RGB led - red light
-DigitalOut ledG(PB_7);  //RGB led - green light 
-DigitalOut ledB(PB_15);  //RGB led - blue light
+DigitalOut ledR(PA_8);		//RGB led - red light
+DigitalOut ledG(PB_7);		//RGB led - green light
+DigitalOut ledB(PB_15);		//RGB led - blue light
 
 // We set the sensor address. For TCS34725 is 0x29 = 0010 1001 (bin) ->> 0101 0010 (bin) = 0x52
 // We shift 1 bit to the left because in I2C protocol slave address is 7-bit. So we discard the 8th bit
-int colorSensor_addr = 0x29 << 1; 
+int colorSensor_addr = 0x29 << 1;
 
 //Registers to obtain and save the values sensed by the color sensor
 char clear_reg[1] = {0xB4}; // 1011 0100 -> 0x34 and we set 1st bit to 1
-char data[8] = {0,0,0,0,0,0,0,0};
+char data[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 int clear_value = 0;
 int red_value = 0;
@@ -47,12 +47,11 @@ void switchLED();
 //bool normalModeI2C = false;
 extern bool mode_change;
 
-
 //INITIALIZE TEMPERATURE AND HUMIDITY SENSOR
 int temperatureSensor_addr = 0x80; // Device ADDR: 0x80 = SI7021 7-bits address shifted one bit left.
 
-char read_temp[1];  //F3
-char read_hum[1];   //F5
+char read_temp[1]; //F3
+char read_hum[1];  //F5
 
 char receive_temp[2];
 char receive_hum[2];
@@ -68,10 +67,10 @@ float min_hum = 100.0;
 float max_hum_hour;
 float min_hum_hour;
 float average_temp_hour = 0.0;
-float average_temp = 0.0;;
+float average_temp = 0.0;
+;
 float average_hum_hour = 0.0;
 float average_hum = 0.0;
-
 
 //INITIALIZE ACCELEROMETER SENSOR
 MMA8451Q acc(PB_9, PB_8, MMA8451_I2C_ADDRESS);
@@ -98,112 +97,136 @@ float min_z_hour = 0.0;
 extern float valueSM;
 extern float valueLight;
 
-
 //Method to initialize the color sensor
-void initializeColorSensor(){
-		
-	//We obtain device ID from ID register (0x12)	
+void initializeColorSensor()
+{
+
+	//We obtain device ID from ID register (0x12)
 	char id_regval[1] = {0x92}; //?1001 0010? (bin)
-	char data[1] = {0}; //?0000 0000?
-  i2c.write(colorSensor_addr,id_regval,1, true);
-  i2c.read(colorSensor_addr,data,1,false); 
-	
+	char data[1] = {0};			//?0000 0000?
+	i2c.write(colorSensor_addr, id_regval, 1, true);
+	i2c.read(colorSensor_addr, data, 1, false);
+
 	//We check that the ID is the TCS34725 one. If it is, we switch off a LED on the board, wait for 2s, and switch on again
-	if (data[0]==0x44) { // 0100 0100 -> Value for the part number (0x44 for TCS34725)
+	if (data[0] == 0x44)
+	{ // 0100 0100 -> Value for the part number (0x44 for TCS34725)
 	}
-	
+
 	//Timing register address 0x01 (0000 0001). We set 1st bit to 1 -> 1000 0001
-  char timing_register[2] = {0x81,0x50}; //0x50 ~ 400ms
-  i2c.write(colorSensor_addr,timing_register,2,false);
+	char timing_register[2] = {0x81, 0x50}; //0x50 ~ 400ms
+	i2c.write(colorSensor_addr, timing_register, 2, false);
 
 	//Control register address 0x0F (0000 1111). We set 1st bit to 1 -> 1000 1111
-  char control_register[2] = {0x8F,0}; //{0x8F, 0x00}, {1000 1111, 0000 0000} -> 1x gain
-  i2c.write(colorSensor_addr,control_register,2,false);	
-		
-	// Enable register address 0x00 (0000 0000). We set 1st bit to 1 -> 1000 0000
-  char enable_register[2] = {0x80,0x03}; //{0x80, 0x03}, {1000 0000, 0000 0011} -> AEN = PON = 1
-  i2c.write(colorSensor_addr,enable_register,2,false);
+	char control_register[2] = {0x8F, 0}; //{0x8F, 0x00}, {1000 1111, 0000 0000} -> 1x gain
+	i2c.write(colorSensor_addr, control_register, 2, false);
 
+	// Enable register address 0x00 (0000 0000). We set 1st bit to 1 -> 1000 0000
+	char enable_register[2] = {0x80, 0x03}; //{0x80, 0x03}, {1000 0000, 0000 0011} -> AEN = PON = 1
+	i2c.write(colorSensor_addr, enable_register, 2, false);
 }
 
 //Method to obtain the values given by the color sensor for each color: clear, red, green and blue
-void readColorSensor(){
-		
+void readColorSensor()
+{
+
 	//Start color sensor
-  i2c.write(colorSensor_addr,clear_reg,1, true);
-  
+	i2c.write(colorSensor_addr, clear_reg, 1, true);
+
 	//Read color sensor
-	i2c.read(colorSensor_addr,data,8, false);
+	i2c.read(colorSensor_addr, data, 8, false);
 }
 
 //Get max value (r,g,b) function
-char getMax(int r, int g, int b) {
-  char result;
-  int max;
-  if (r < g){
-    max = g; 
-    result = 'g';  
-  } else {
-    max= r;
-    result = 'r';
-  }
-  if (max < b){
-    result = 'b';
-  }
-  return result;
+char getMax(int r, int g, int b)
+{
+	char result;
+	int max;
+	if (r < g)
+	{
+		max = g;
+		result = 'g';
+	}
+	else
+	{
+		max = r;
+		result = 'r';
+	}
+	if (max < b)
+	{
+		result = 'b';
+	}
+	return result;
 }
 
-char obtainDominantColour(char value){
-	
+char obtainDominantColour(char value)
+{
+
 	char dc;
-	
-	if(value == 'r'){
+
+	if (value == 'r')
+	{
 		contRed++;
-	}else if(value == 'b'){
+	}
+	else if (value == 'b')
+	{
 		contBlue++;
-	}else{
+	}
+	else
+	{
 		contGreen++;
 	}
-	
-	if((contRed > contBlue) and (contRed > contGreen)){
+
+	if ((contRed > contBlue) and (contRed > contGreen))
+	{
 		//Red dominant color
 		dc = 'r';
-	}else if((contBlue > contRed) and (contBlue > contGreen)){
+	}
+	else if ((contBlue > contRed) and (contBlue > contGreen))
+	{
 		//Blue dominant color
 		dc = 'b';
-	}else if ((contGreen > contRed) and (contGreen > contBlue)){
+	}
+	else if ((contGreen > contRed) and (contGreen > contBlue))
+	{
 		//Green dominant color
 		dc = 'g';
-	}else{
+	}
+	else
+	{
 		dc = 'y';
 	}
 	return dc;
 }
 
-
 //Method to switch ON the led with the max colour sensed by the color sensor
-void switchLedTestMode(char max){
-	
+void switchLedTestMode(char max)
+{
+
 	//Switch OFF all colours
 	ledR.write(1);
-  ledG.write(1);
-  ledB.write(1);
-  
+	ledG.write(1);
+	ledB.write(1);
+
 	//Switch ON the max colour
-	if (max == 'r'){ 
+	if (max == 'r')
+	{
 		ledR.write(0);
-     //pc.printf("Red\r\n");
-  } else if(max == 'g'){
+		//pc.printf("Red\r\n");
+	}
+	else if (max == 'g')
+	{
 		//pc.printf("Green\r\n");
-    ledG.write(0);
-  } else{
+		ledG.write(0);
+	}
+	else
+	{
 		//pc.printf("Blue\r\n");
-    ledB.write(0);
+		ledB.write(0);
 	}
 }
 
-
-void switchLedNormalMode(){
+void switchLedNormalMode()
+{
 	//Yellow    R255 G255 B0     -----------  HUMIDITY       25 A 75
 	//Green     R0   G255 B0     -----------  TEMPERATURE   -10 A 50
 	//Cyan      R0   G255 B255   -----------  AMBIENT LIGHT  10 A 90
@@ -211,42 +234,55 @@ void switchLedNormalMode(){
 	//Magenta   R255 G0   B255   -----------  COLOUR          GREEN
 	//Red       R255 G0   B0     -----------  ACCELEROMETER   ENTRE 1 Y -1 (PARA CUALQUIER EJE)
 	//White     R255 G255 B255   -----------  OK
-	
+
 	//Switch OFF all colours
 	ledR.write(1);
-  ledG.write(1);
-  ledB.write(1);
-	if(hum>75.0 or hum<25.0){
+	ledG.write(1);
+	ledB.write(1);
+	if (hum > 75.0 or hum < 25.0)
+	{
 		//Yellow
 		ledR.write(0);
 		ledG.write(0);
 		ledB.write(1);
-	}else if(temp<-10.0 or temp>50.0){
+	}
+	else if (temp < -10.0 or temp > 50.0)
+	{
 		//Green
 		ledR.write(1);
 		ledG.write(0);
 		ledB.write(1);
-	}else if(valueLight<10.0 or valueLight>90.0){
+	}
+	else if (valueLight < 10.0 or valueLight > 90.0)
+	{
 		//Cyan
 		ledR.write(1);
 		ledG.write(0);
 		ledB.write(0);
-	}else if(valueSM<10.0 or valueSM>90.0){
+	}
+	else if (valueSM < 10.0 or valueSM > 90.0)
+	{
 		//Blue
 		ledR.write(1);
 		ledG.write(1);
 		ledB.write(0);
-	}else if(max_color_value != 'g'){
+	}
+	else if (max_color_value != 'g')
+	{
 		//Magenta
 		ledR.write(0);
 		ledG.write(1);
 		ledB.write(0);
-	}else if((x_accel<-1.0 or x_accel>1.0) or (y_accel<-1.0 or y_accel>1.0) or (z_accel<-1.0 or z_accel>1.0)){
+	}
+	else if ((x_accel < -1.0 or x_accel > 1.0) or (y_accel < -1.0 or y_accel > 1.0) or (z_accel < -1.0 or z_accel > 1.0))
+	{
 		//Red
 		ledR.write(0);
 		ledG.write(1);
 		ledB.write(1);
-	}else{
+	}
+	else
+	{
 		//White
 		ledR.write(0);
 		ledG.write(0);
@@ -254,114 +290,137 @@ void switchLedNormalMode(){
 	}
 }
 
-void obtainTemperature(){
-	read_temp[0] = 0xE3;  //0xE3
-	
-	i2c.write(temperatureSensor_addr, read_temp, 1,true);
+void obtainTemperature()
+{
+	read_temp[0] = 0xE3; //0xE3
+
+	i2c.write(temperatureSensor_addr, read_temp, 1, true);
 	//wait_ms(100);
 	i2c.read(temperatureSensor_addr, receive_temp, 2); // Receive MSB = receive_temp[0], then LSB = receive_temp[1]
-	temp = ((((receive_temp[0] <<8) + receive_temp[1]) * 175.72) / 65536.0) - 46.85;
+	temp = ((((receive_temp[0] << 8) + receive_temp[1]) * 175.72) / 65536.0) - 46.85;
 }
 
-void obtainHumidity(){
-	read_hum[0] = 0xE5;  //0xE5
-	
+void obtainHumidity()
+{
+	read_hum[0] = 0xE5; //0xE5
+
 	i2c.write(temperatureSensor_addr, read_hum, 1, true);
 	//wait_ms(100);
 	i2c.read(temperatureSensor_addr, receive_hum, 2); // Receive MSB = receive_hum[0], then LSB = receive_hum[1]
-	hum = ((((receive_hum[0] <<8) + receive_hum[1]) * 125.0) / 65536.0) - 6.0;
+	hum = ((((receive_hum[0] << 8) + receive_hum[1]) * 125.0) / 65536.0) - 6.0;
 }
 
-void obtainMaxOrMinTemp(float value){
-	if(value>max_temp){
+void obtainMaxOrMinTemp(float value)
+{
+	if (value > max_temp)
+	{
 		max_temp = value;
-	}else if(value<min_temp){
+	}
+	else if (value < min_temp)
+	{
 		min_temp = value;
 	}
 }
 
-void obtainMaxOrMinHum(float value){
-	if(value>max_hum){
+void obtainMaxOrMinHum(float value)
+{
+	if (value > max_hum)
+	{
 		max_hum = value;
-	}else if(value<min_hum){
+	}
+	else if (value < min_hum)
+	{
 		min_hum = value;
 	}
 }
 
-void obtainMaxOrMinXAxis(float value){
-	if(value > max_x){
+void obtainMaxOrMinXAxis(float value)
+{
+	if (value > max_x)
+	{
 		max_x = value;
 	}
-	else if(value < min_x){
+	else if (value < min_x)
+	{
 		min_x = value;
 	}
 }
-void obtainMaxOrMinYAxis(float value){
-	if(value > max_y){
+void obtainMaxOrMinYAxis(float value)
+{
+	if (value > max_y)
+	{
 		max_y = value;
 	}
-	else if(value < min_y){
+	else if (value < min_y)
+	{
 		min_y = value;
 	}
 }
-void obtainMaxOrMinZAxis(float value){
-	if(value > max_z){
+void obtainMaxOrMinZAxis(float value)
+{
+	if (value > max_z)
+	{
 		max_z = value;
 	}
-	else if(value < min_z){
+	else if (value < min_z)
+	{
 		min_z = value;
 	}
 }
 
-
-
 //I2C THREAD
-void I2C_thread(){
-	
+void I2C_thread()
+{
+
 	//INITIALIZE COLOR SENSOR
 	initializeColorSensor();
 	ledColour = 1;
 
-	
-	while(true){
-		
-		Thread::wait(tiempo);  //In miliseconds
+	while (true)
+	{
+
+		Thread::wait(tiempo); //In miliseconds
 		//Thread::wait(30000);  //In miliseconds. Normal mode
-		
+
 		//OBTAIN COLOR SENSOR VALUES
 		readColorSensor();
-	
+
 		clear_value = ((int)data[1] << 8) | data[0];
 		red_value = ((int)data[3] << 8) | data[2];
 		green_value = ((int)data[5] << 8) | data[4];
 		blue_value = ((int)data[7] << 8) | data[6];
-		
+
 		//Obtain max value
 		max_color_value = getMax(red_value, green_value, blue_value);
-		if(!mode_change){
+		if (!mode_change)
+		{
 			switchLedTestMode(max_color_value);
-		}else{
+		}
+		else
+		{
 			//Normal mode
 			switchLedNormalMode();
 		}
-	
+
 		//OBTAIN ACCELEROMETER VALUES
 		//Read and print the values at the same time
 		x_accel = acc.getAccX();
 		y_accel = acc.getAccY();
 		z_accel = acc.getAccZ();
-		
-		//OBTAIN HUMIDITY AND TEMPERATURE VALUES		
+
+		//OBTAIN HUMIDITY AND TEMPERATURE VALUES
 		obtainTemperature();
 		obtainHumidity();
-		
+
 		//temp = ((((receive_temp[0] <<8) + receive_temp[1]) * 175.72) / 65536.0) - 46.85;	// Conversion based on Datasheet
 		//hum = ((((receive_hum[0] <<8) + receive_hum[1]) * 125.0) / 65536.0) - 6.0;				// Conversion based on Datasheet
-		
-		if(mode_change){
+
+		if (mode_change)
+		{
 			contadorI2C++;
 			//Reset contador when one hour passes
-			if(contadorI2C > contador_maxI2C){
+			if (contadorI2C > contador_maxI2C)
+			{
 				contadorI2C = 1;
 				//Reset contadores de color cuando pase una hora
 				contRed = 0;
@@ -399,18 +458,18 @@ void I2C_thread(){
 			}
 			//COLOR SENSOR
 			dominantColour = obtainDominantColour(max_color_value);
-			
+
 			//ACCELEROMETER
 			//Normal mode
 			obtainMaxOrMinXAxis(x_accel);
 			obtainMaxOrMinYAxis(y_accel);
 			obtainMaxOrMinZAxis(z_accel);
-			
+
 			//OBTAIN HUMIDITY AND TEMPERATURE VALUE
 			obtainMaxOrMinTemp(temp);
 			obtainMaxOrMinHum(hum);
-			average_temp = (average_temp*(contadorI2C-1)+temp)/contadorI2C;
-			average_hum = (average_hum*(contadorI2C-1)+hum)/contadorI2C;
+			average_temp = (average_temp * (contadorI2C - 1) + temp) / contadorI2C;
+			average_hum = (average_hum * (contadorI2C - 1) + hum) / contadorI2C;
 		}
 	}
 }
